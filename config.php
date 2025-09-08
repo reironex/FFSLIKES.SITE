f5905fa4af1b897ebaf9c094259f72352b599251
reironex <nexreiro@gmail.com>
2025-09-08 21:13:06 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
 $host = "db4free.net";          // palitan mo depende sa DB host (default ng db4free)
-$username = "your_db_username"; // palitan mo ng db4free username mo
-$password = "your_db_password"; // palitan mo ng db4free password mo
-$dbname = "your_db_name";       // palitan mo ng db4free database name mo
+$username = "johnalpay"; // palitan mo ng db4free username mo
+$password = "babykojohn15y"; // palitan mo ng db4free password mo
+$dbname = "John Albert Alpay";       // palitan mo ng db4free database name mo
 
 // User agent string (ginagamit sa mga cURL request mo)
 $useragent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36";
